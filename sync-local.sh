080ddf1ef2911cbeb1325213dfb9a3e10057b93d
neio <marcon.dylan.fr@gmail.com>
2025-11-23 22:44:46 +0100

    Update sync local

--- sync-local.sh
+++ sync-local.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # This program will put any config in "auto" dir into corresponding directories
 rm -rf ./env/.config/nvim
 echo "Deleted local env/.config/nvim folder"
 cp -r ~/.config/nvim ./env/.config/nvim
 echo "Moved ~/.config/nvim to env/.config/nvim"
 git add ./env/.config/nvim*
-git commit -m "Updated nvim config"
+git commit -m "${*:-Update nvim config}"
